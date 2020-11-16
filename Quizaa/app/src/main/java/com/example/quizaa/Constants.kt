a7cd7da6b113642f7d8e715222dca2f9cf9d6702
chanchaiIssara <68723442+chanchaiIssara@users.noreply.github.com>
2020-11-16 21:39:49 +0700

    2

--- Quizaa/app/src/main/java/com/example/quizaa/Constants.kt
+++ Quizaa/app/src/main/java/com/example/quizaa/Constants.kt
@@ -8,7 +8,7 @@ object Constants {
         val que1 = Question(
             1, "What does thid flag belong to?",
             R.drawable.ic_flag_of_argentina,
-            "Aregentina",
+            "Argentina",
             "Australia",
             "Armenia",
             "Austria",
@@ -16,6 +16,58 @@ object Constants {
         )
 
         questionList.add(que1)
+
+        val que2 = Question(
+            2, "What does thid flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "Bahamas",
+            "Belgium",
+            "Benin",
+            "Bolivia",
+            2
+        )
+
+        questionList.add(que2)
+
+        val que3 = Question(
+            3, "What does thid flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "Brazil",
+            "Belgium",
+            "Benin",
+            "Bolivia",
+            1
+        )
+
+        questionList.add(que1)
+
+        val que4 = Question(
+            4, "What does thid flag belong to?",
+            R.drawable.ic_flag_of_egyptian,
+            "Eritrea",
+            "Equatorial Guinea",
+            "El Salvador",
+            "Egyptian",
+            4
+        )
+
+        questionList.add(que4)
+
+        val que5 = Question(
+            5, "What does thid flag belong to?",
+            R.drawable.ic_flag_of_france,
+            "Fiji",
+            "Finland",
+            "France",
+            "Fuji",
+            3
+        )
+
+        questionList.add(que3)
+
         return questionList
+
+
+
     }
 }
